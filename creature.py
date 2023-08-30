301b2e9e76f228219d83b645dab0a12a0c68ba8c
ProfessorRich <61430939+ProfessorRich@users.noreply.github.com>
2023-08-30 14:38:00 +0100

    Begin creation of Creature class.

--- creature.py
+++ creature.py
@@ -1,1 +1,3 @@
-
+class Creature:
+    def __init__(self, name, armor_class, max_hit_points, attacks, ability_scores=None, hit_points=None, immunities=None, resistances=None, vulnerabilities=None)
+      
